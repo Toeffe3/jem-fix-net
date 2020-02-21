60cbd55108d08d490f67c287d902c0ae658cad03
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2020-02-21 23:09:39 +0100

    Testing github-actions

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-  include_once("login.php");
+
 ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+  </head>
+  <body>
+    <h1>Welcome to the site</h1>
+    <h3>This is a test!</h3>
+  </body>
+</html>
