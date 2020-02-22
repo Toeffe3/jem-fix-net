1ce0dea333dc939bc530db785fad3cb8ecfe6abf
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2020-02-22 18:39:39 +0100

    Code Sniffer rettelser

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-	declare(strict_types = 1);
-	include_once("login.php");
+  declare(strict_types = 1);
+  include_once("login.php");
+?>
--- login.php
+++ login.php
@@ -1,3 +1,4 @@
 <?php
-	declare(strict_types = 1);
-	include_once("login_form.html");
+  declare(strict_types = 1);
+  include_once("login_form.html");
+?>
