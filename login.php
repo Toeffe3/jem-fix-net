634ff691f644967af7031e1602974bafefc05de2
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2020-02-24 09:29:50 +0100

    Minor fixes

--- login.php
+++ login.php
@@ -1,42 +1,40 @@
 <?php
-session_start();
+session_start()
 include __DIR__."/dbcon.php";
-if(isset($_GET["logout"])) {
+
+if (isset($_GET["logout"])) {
   session_start();
   session_unset();
-  session_destroy():
+  session_destroy();
   $_SESSION = array();
-  header('Location: http://jemfixnet.dk/');
+  header('Location: http://jemfixnet.dk?login=logout');
 }
-?>
-<?php
-if(!empty($_POST["email"]) && !empty($_POST["pass"]) && empty($_POST["user"])) {
+
+if (!empty($_POST["email"]) && !empty($_POST["pass"])) {
   $email = $_POST['email'];
   $pass = md5($_POST['pass']);
   $login = mysqli_query($conn, "SELECT * FROM `users` WHERE `mail` = '$email' AND `password` = '$pass'");
   if (mysqli_num_rows($login) == 1) {
     $acc = mysqli_fetch_assoc($login);
     $_SESSION["user"] = $acc["id"];
-    $_SESSION["name"] = $acc["name"];
-    header('Location: http://jemfixnet.dk/');
+    $_SESSION["email"] = $acc["email"];
+    header('Location: http://jemfixnet.dk?login=sucess');
   } else {
-    header('Location: http://jemfixnet.dk/');
+    header('Location: http://jemfixnet.dk?login=failure');
   }
-}else if(!empty($_SESSION["user"])) {
- ?>
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Login</title>
-  </head>
-  <body>
-    <form class="" action="index.html" method="post">
-      <input type="text" name="" value="">
-      <input type="password" name="" value="">
-      <input type="submit" name="" value="">
-    </form>
-  </body>
-</html>
-<?php } ?>
+} else if (!empty($_SESSION["user"])) {
+  echo '<!DOCTYPE html>';
+  echo '<html lang="en" dir="ltr">';
+  echo '<head>';
+  echo '<meta charset="utf-8">';
+  echo '<title>Login</title>';
+  echo '</head>';
+  echo '<body>';
+  echo '<form class="" action="login.php" method="post">';
+  echo '<input type="email" name="email" value="">';
+  echo '<input type="password" name="" value="">';
+  echo '<input type="submit" name="" value="">';
+  echo '</form>';
+  echo '</body>';
+  echo '</html>';
+}
