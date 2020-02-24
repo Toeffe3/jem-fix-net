03f79dd09de7e875de27455b90ea308ad59c8e5e
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2020-02-24 19:32:28 +0100

    Delete upload.bat

--- upload.bat
+++
@@ -1,7 +0,0 @@
-REM Script to upload files to webserver
-REM Uses a private "transfer.ftp" file that contains login
-
-@setlocal enableextensions enabledelayedexpansion
-FTP -n -i -s:transfer.ftp jemfixnet.dk
-ENDLOCAL
-TIMEOUT 10 >NUL
