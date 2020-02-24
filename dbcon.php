2a7d70d69e2c352c2c50e6816c234b931cc27915
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2020-02-24 23:13:12 +0100

    SPACES

--- dbcon.php
+++ dbcon.php
@@ -1,5 +1,5 @@
 <?php
-	$passfile = fopen("pass", "r") or die("Unable to rechive password!");
-	$pass = fread($passfile, filesize("pass"));
-	$conn = mysqli_connect("cp01.nordicway.dk", "jemfixne_webpage", $pass, "jemfixne_public");
-	if (!$conn) die("Connection failed: " . mysqli_error($conn));
+  $passfile = fopen("pass", "r") or die("Unable to rechive password!");
+  $pass = fread($passfile, filesize("pass"));
+  $conn = mysqli_connect("cp01.nordicway.dk", "jemfixne_webpage", $pass, "jemfixne_public");
+  if (!$conn) die("Connection failed: " . mysqli_error($conn));
