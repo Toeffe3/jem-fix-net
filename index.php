51cfffb74b01c3b39ec57e73c1de060973f323af
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2020-03-10 13:41:21 +0100

    login fix

--- index.php
+++ index.php
@@ -7,10 +7,9 @@
     include_once "header.php";
 
     if(isset($_SESSION["user"])) {
-        if($_GET["login"]=="sucess") echo '<script>window.href.location="?nyheder"</script>';
-        if(isset($_GET["space"])) include_once "subsites/space.php";
+        if($_GET["login"]=="sucess" || isset($_GET["nyheder"])) include_once "subsites/nyheder.php";
+        else if(isset($_GET["space"])) include_once "subsites/space.php";
         else if(isset($_GET["cyklisk"])) include_once "subsites/cyklisk.php";
-        else if(isset($_GET["nyheder"])) include_once "subsites/nyheder.php";
         else if(isset($_GET["leder"])) include_once "subsites/leder.php";
         else if(isset($_GET["hr"])) include_once "subsites/hr.php";
     } else include_once "subsites/login.php";
