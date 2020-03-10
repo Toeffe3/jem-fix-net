cdbb7991ea949855407fb0654069a418129f1503
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2020-03-10 11:47:18 +0100

    no header

--- subsites/login.php
+++ subsites/login.php
@@ -4,7 +4,7 @@ if (isset($_GET["logout"])) {
     session_unset();
     session_destroy();
     $_SESSION = array();
-    header('Location: http://jemfixnet.dk?login=logout');
+    echo '<script>window.href.location="http://jemfixnet.dk?login=logout"</script>'
 } else if (!empty($_POST["initials"]) && !empty($_POST["password"])) {
     $initi = $_POST['initials'];
     $pass = md5($_POST['password']);
@@ -13,8 +13,8 @@ if (isset($_GET["logout"])) {
         $acc = mysqli_fetch_assoc($login);
         $_SESSION["user"] = $acc["id"];
         $_SESSION["initials"] = $acc["initials"];
-        header('Location: http://jemfixnet.dk?login=sucess');
-    } else header('Location: http://jemfixnet.dk?login=failure');
+        echo '<script>window.href.location="http://jemfixnet.dk?login=sucess"</script>'
+    } else echo '<script>window.href.location="http://jemfixnet.dk?login=failure"</script>';
 } else {?>
     <div class="bg-grey dark white" id="page">
         <form class="" action="index.php" method="post">
