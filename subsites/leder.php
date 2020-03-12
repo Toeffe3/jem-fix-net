5143ff2212cf4f18f8bb16d870434baaecf8fef5
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2020-03-12 15:10:53 +0100

    Patching

--- subsites/leder.php
+++ subsites/leder.php
@@ -1,25 +1,26 @@
-<h1>Leder</h1>
 <?php
-    $leaderpost = mysqli_fetch_assoc(mysqli_query($conn,"SELECT * FROM `posts` INNER JOIN `employees` ON `userid` = `id` WHERE `from` = 1"));
+    $leaderpost = mysqli_query($conn,"SELECT * FROM `posts` INNER JOIN `employees` ON `userid` = `id` WHERE `from` = 1");
 ?>
 <div id="page" class="bg-gray">
-    <div class="small box bg-white">
+    <div class="box bg-white">
         Nyt fra ledelsen
         <div class="posts">
-            <?php foreach ($leaderpost => $post) {
-                echo '<div class="post">['.$post["initials"].']: <a href=?nyheder&id='.$post["id"].'>'.$post["title"].'</a></div>';
-            } ?>
+            <?php
+                while($post = mysqli_fetch_assoc($leaderpost)) {
+                    echo '<div class="post">['.$post["initials"].']: <a href=?nyheder&id='.$post["id"].'>'.$post["title"].'</a></div>';
+                }
+            ?>
         </div>
     </div>
-    <div class="panel bg-yellow">
+    <div class="tall box bg-yellow">
         Hurtig adgang
-        <div class="functions">
-          <a href="#">Opret ny bruger</a>
-          <a href="#">Opret ny post</a>
-          <a href="#">Rediger post</a>
-          <a href="#">Upload dokument</a>
-          <a href="#">Rediger tags(Mapper)</a>
-          <a href="#">Fjern bruger</a>
+        <div id="functions">
+          <li><a href="#">Opret ny bruger</a></li>
+          <li><a href="#">Opret ny post</a></li>
+          <li><a href="#">Rediger post</a></li>
+          <li><a href="#">Upload dokument</a></li>
+          <li><a href="#">Rediger tags(Mapper)</a></li>
+          <li><a href="#">Fjern bruger</a></li>
         </div>
     </div>
 </div>
--- subsites/nyheder.php
+++ subsites/nyheder.php
@@ -1,37 +1,37 @@
 <?php
-  $leaderpost = mysqli_fetch_assoc(mysqli_query($conn,"SELECT * FROM `posts` INNER JOIN `employees` ON `userid` = `id` WHERE `from` = 1"));
-    $post = mysqli_fetch_assoc(mysqli_query($conn,"SELECT * FROM `posts` INNER JOIN `employees` ON `userid` = `id` WHERE `from` = 0"));
-    $cykliskpost = mysqli_fetch_assoc(mysqli_query($conn,"SELECT * FROM `posts` INNER JOIN `employees` ON `userid` = `id` WHERE `from` = 2"));
+    $posts = mysqli_query($conn, "SELECT * FROM `posts` INNER JOIN `employees` ON `userid` = `id` WHERE `from` = 0");
+    $leaderpost = mysqli_query($conn, "SELECT * FROM `posts` INNER JOIN `employees` ON `userid` = `id` WHERE `from` = 1");
+    $cykliskpost = mysqli_query($conn, "SELECT * FROM `posts` INNER JOIN `employees` ON `userid` = `id` WHERE `from` = 2");
 ?>
 <div id="page" class="bg-gray">
     <div class="small box bg-white">
         Aktuelt fra indkøb
         <div class="posts">
-            <?php foreach ($cykliskpost => $post) {
-                echo '<div class="post">['.$post["initials"].']: <a href=?nyheder&id='.$post["id"].'>'.$post["title"].'</a></div>';
+            <?php while($post = mysqli_fetch_assoc($cykliskpost)) {
+                echo '<div class="post">['.$post["initials"].']: <a href=?nyheder&id='.$post["post"].'>'.$post["title"].'</a></div>';
             } ?>
         </div>
     </div>
     <div class="small box bg-white">
         Øvrige nyheder
         <div class="posts">
-            <?php foreach ($posts => $post) {
-                echo '<div class="post">['.$post["initials"].']: <a href=?nyheder&id='.$post["id"].'>'.$post["title"].'</a></div>';
+            <?php while($post = mysqli_fetch_assoc($posts)) {
+                echo '<div class="post">['.$post["initials"].']: <a href=?nyheder&id='.$post["post"].'>'.$post["title"].'</a></div>';
             } ?>
         </div>
     </div>
-    <div class="panel bg-yellow">
+    <div class="tall box bg-yellow">
         Min butik
     </div>
-    <div class="box bg-white">
+    <div class="large box bg-white">
         Nyt fra ledelsen
         <div class="posts">
-            <?php foreach ($leaderpost => $post) {
-                echo '<div class="post">['.$post["initials"].']: <a href=?nyheder&id='.$post["id"].'>'.$post["title"].'</a></div>';
+            <?php while($post = mysqli_fetch_assoc($leaderpost)) {
+                echo '<div class="post">['.$post["initials"].']: <a href=?nyheder&id='.$post["post"].'>'.$post["title"].'</a></div>';
             } ?>
         </div>
     </div>
-    <div class="panel bg-yellow">
+    <div class="tall box bg-yellow">
         Hurtig adgang
     </div>
 </div>
