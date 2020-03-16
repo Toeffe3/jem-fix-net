6b5440bd517987c33a3d1a502063e49ea817b55b
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2020-03-17 00:04:31 +0100

    cyklisk subpage

--- subsites/cyklisk.php
+++ subsites/cyklisk.php
@@ -1,11 +1,38 @@
 <?php
-    
+    $cyklisker = mysqli_query($conn, "SELECT * FROM `cyklisks` ORDER BY `mustdate` ASC, `start` ASC");
 ?>
 <div id="page" class="bg-gray">
-    <div class="small box bg-white">
+    <div class="full box bg-white">
         Cyklisker
-        <div class="posts">
-            
-        </div>
+        <table style="width:100%">
+            <tr>
+                <th>Fuldendt</th>
+                <th>Uge</th>
+                <?php
+                    for($i = intval(date("W")); $i < (intval(date("W"))+15); $i++)
+                        echo '<th>'.$i.'</th>';
+                ?>
+            </tr>
+
+            <?php
+                while($cykliskider = mysqli_fetch_assoc($cyklisker)) {
+                    $spaces = preg_split("/,/",$cykliskider["spaces"]);
+                    $start = DateTime::createFromFormat('Y-m-d', $cykliskider["start"]);
+                    $mustdate = DateTime::createFromFormat('Y-m-d', $cykliskider["mustdate"]);
+                    
+                    foreach($spaces as $space) {
+                        echo '<tr>';
+                        echo '<th>X</th><th>'.$space.'</th>';
+                        for($i = intval(date("W")); $i < (intval(date("W"))+15); $i++) {
+                            if($i == intval($mustdate->format('W'))) echo '<th>S</th>';
+                            else if($i == intval($start->format('W'))) echo '<th>F</th>';
+                            else '<th></th>';
+						}
+                        echo '</tr>';
+				    }
+                }
+            ?>
+
+        </table>
     </div>
 </div>
