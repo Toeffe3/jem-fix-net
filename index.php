0d067a4c1cb6d56274df8bcd69e6e1b28c4120d3
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2020-03-18 00:23:51 +0100

    Reworked redirection system

--- index.php
+++ index.php
@@ -2,12 +2,24 @@
     declare(strict_types = 1);
     session_start();
     if (!defined(__DIR__)) define(__DIR__, dirname(__FILE__));
-    date_default_timezone_set("UTC"); //Uverrides default Europe/Copenhagen (PHP throws errors that it is invalid)
     include __DIR__."/dbcon.php";
+    date_default_timezone_set("UTC"); //Overrides default Europe/Copenhagen (PHP throws errors that it is invalid)
+    
+    $url = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    if(parse_url($url, PHP_URL_QUERY) != parse_url($_SESSION["prevpage"], PHP_URL_QUERY)) {
+            $prevpage = $_SESSION["prevpage"];
+        $_SESSION["prevpage"] = $url;
+	}
 
     include_once "header.php";
 
     if(isset($_SESSION["user"])) {
+
+        if(isset($_GET["users"])) include_once "functions/editUser.php";
+        else if(isset($_GET["posts"])) include_once "functions/editPost.php";
+        else if(isset($_GET["documents"])) include_once "functions/editDocument.php";
+		else if(isset($_GET["folders"])) include_once "functions/editFolder.php";
+		else if(isset($_GET["pin"])) include_once "functions/editPin.php";
         
         if(isset($_GET["nyheder"]) || isset($_GET["login"]) && $_GET["login"] == "sucess")
             if(!empty($_GET["id"])) include_once "subsites/post.php";
@@ -17,6 +29,7 @@
         else if(isset($_GET["cyklisk"])) include_once "subsites/cyklisk.php";
         else if(isset($_GET["leder"])) include_once "subsites/leder.php";
         else if(isset($_GET["hr"])) include_once "subsites/hr.php";
+        else include_once "subsites/nyheder.php";
     } else include_once "subsites/login.php";
 
     include_once "footer.php";
