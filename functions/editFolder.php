f2231a8542378f8c5794c797d54cdc8d30cceb84
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2020-03-22 15:57:32 +0100

    Added tags (folder managment for uploads)

---
+++ functions/editFolder.php
@@ -0,0 +1,52 @@
+<?php
+    switch ($_GET["folder"]) {
+    	case 'new':
+            $action = $url;
+            $inputs = [
+                "tagname" => ["text","Navn på tag", 1],
+                "redirect" => ["hidden",$prevpage, 0],
+                "submit" => ["submit", "Opret", 0]
+		    ];
+            if($_POST["submit"]) {
+                mkdir(__DIR__."/../documents/".$_POST["tagname"]);
+                //TODO: Sucess/fail feedback
+                echo "<script>window.location.href='".$_POST["redirect"]."'</script>";
+			}
+            break;
+
+    	case 'edit':
+            $current_tags = array();
+            $directory = dir(__DIR__."/../documents/");
+            while(false !== ($folder = $directory->read()))
+                if($folder != "." && $folder != ".." && !preg_match('/\./', $folder))
+                    $current_tags[$folder] = $folder;
+            
+            $action = $url;
+            $inputs = [
+                "seltag" => ["select", $current_tags, 1],
+                "redirect" => ["hidden",$prevpage, 0]
+            ];
+
+            if(isset($_GET["remove"])) {
+                $inputs["confirm"] = ["text", "Skriv navnet for at bekræfte", 1];
+                $inputs["delete"] = ["submit", "Fjern (Slet alle dokumenter med dette tag)", 0];
+            } else {
+                $inputs["newname"] = ["text", "Nyt navn", 0];
+                $inputs["access"] = ["select", [0=>"Alle",1=>"Brt. Ansatte",2=>"Leder",3=>"Butikschef"], 0];
+                $inputs["submit"] = ["submit", "Ændre", 0];
+            }
+
+            //TODO: Sucess/fail feedback
+            if($_POST["submit"]) {
+                if(!empty($_GET["newname"])) rename(__DIR__."/../documents/".$_POST["seltag"], __DIR__."/../documents/".$_POST["newname"]);
+                if(!empty($_GET["access"])) echo "SQL skal indsættes her! ".$_GET["access"];
+                echo "<script>window.location.href='".$_POST["redirect"]."'</script>";
+			} else if($_POST["delete"]) {
+                if($_POST["seltag"] == $_POST["confirm"])
+                    rmdir(__DIR__."/../documents/".$_POST["seltag"]);
+                echo "<script>window.location.href='".$_POST["redirect"]."'</script>";
+			}
+            break;
+    }
+    
+    include_once "overlay.php";
