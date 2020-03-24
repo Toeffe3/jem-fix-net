7505ee3f58f1a11ff7e5dd8348cb1528a00115c2
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2020-03-24 20:59:15 +0100

    Added richtext support with markdown for PHP

--- functions/overlay.php
+++ functions/overlay.php
@@ -2,21 +2,39 @@
 
 <div id="overlay">
 	<form method="post" action="<?php echo $action; ?>" enctype="multipart/form-data">
-		<p>Dette er en fyldtekst</p>
-		<?php
-			foreach($inputs as $key => $value) {
-				$key = preg_replace("/\@\d*/","",$key);
-				if($value[0]=="select") {
-					echo $value[2].'<select name="'.$key.'">';
-					foreach($value[1] as $okey => $option) {
-						echo '<option value="'.$okey.'">'.$option.'</option>';
-					}
-					echo '</select>'.($value[3]==1?"*":"").'<br>';
-				} else {
-					echo '<input type="'.$value[0].'" name="'.$key.'" value="'.$value[1].'" placeholder="'.$value[2].'" '.($value[3]==1?"required":"").'" />'.($value[3]==1?"*":"").'<br>';
-				}
+		<table>
+			<?php
+				foreach($inputs as $key => $value) {
+					$key = preg_replace("/\@\d*/","",$key);
 
-			}
-		?>
+					switch($value[0]) {
+						case "info":
+							echo '<tr><td colspan="2">'.$value[1].'</td></tr>';
+							break;
+
+						case "select":
+							echo "<tr><td>".$value[2].': </td><td><select name="'.$key.'">';
+							foreach($value[1] as $okey => $option) {
+								echo '<option value="'.$okey.'">'.$option.'</option>';
+							}
+							echo '</select>'.($value[3]==1?"*":"").'<td></tr>';
+							break;
+
+							case "textarea":
+								echo '<tr><td>'.$value[2].': </td></tr><tr><td colspan="2"><textarea name="'.$key.'"></textarea></td></tr>';
+								break;
+							
+							case "richtext":
+								echo '<tr><td>'.$value[2].': </td></tr><tr><td colspan="2"><textarea class="rich" name="'.$key.'"></textarea></td></tr>';
+								break;
+
+						default:
+							echo "<tr>".($value[2]==""?"":"<td>".$value[2].": </td>").'<td'.($value[2]==""?" colspan=2":"").'><input type="'.$value[0].'" name="'.$key.'" value="'.$value[1].'" placeholder="'.$value[2].'" '.($value[3]==1?"required":"").'" />'.($value[3]==1?"*":"").'</td></tr>';
+							break;
+					}
+
+				}
+			?>
+		</table>
 	</form>
 </div>
