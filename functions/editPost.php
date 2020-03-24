56e190e0b0b37f4926c156f1635ea5050fc23c6f
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2020-03-24 12:27:52 +0100

    Added interface to 'new post'

--- functions/editPost.php
+++ functions/editPost.php
@@ -1,49 +1,66 @@
 <?php
     //TODO: Add permission check
 
-    switch ($_GET["posts"]) {
+    switch ($_GET["post"]) {
     	case 'new':
-            if(!empty($_SESSION["initials"]) && !empty($_POST["from"]) && !empty($_POST["title"]) && !empty($_POST["text"])) {
-                
-                $id = mysqli_fetch_assoc(mysqli_querry($conn, "SELECT `id` from `employees` WHERE `initiales` ='".$_SESSION["initials"]."' LIMIT 1"));
-                if(mysqli_querry($conn, "INSERT INTO `posts` (`userid`, `from`, `title`, `text`) VALUES (".$id.", ".$_POST["from"].", ".$_POST["title"].", ".$_POST["text"].")"))
-                     echo "Opslag oprettet";
-		        else echo "Fejl ved oprettelse af post<br>".mysqli_error($conn); 
 
-	        } else echo "Alle felter er ikke blevet udfyldt"; break;
+            //kajhkjsh
+            $categories = [
+                0 => "Øvrige",
+                1 => "Ledelsen",
+                2 => "Indkøb"
+            ];
+
+            $action = $url;
+            $inputs = [
+                "title" => ["text","","Titel på opslag", 1],
+                "category" => ["select", $categories,"Kategori: ", 1],
+                "text" => ["text","", "Brødtekst", 1],
+                "redirect" => ["hidden", $prevpage, "", 0],
+                "submit" => ["submit", "Opret", "", 0]
+		    ];
+            
+
+            if($_POST["submit"]) {
+                if(!empty($_SESSION["user"]) && !empty($_POST["category"]) && !empty($_POST["title"]) && !empty($_POST["text"])) {
+                    if(mysqli_query($conn, "INSERT INTO `posts` (`userid`, `from`, `title`, `text`) VALUES ('".$_SESSION["user"]."', '".$_POST["category"]."', '".$_POST["title"]."', '".$_POST["text"]."')"))
+                         echo "<script>window.location.href='".$_POST["redirect"]."'</script>";
+		            else echo "<script>alert('Fejl ved oprettelse af opslag')</script>";
+                }
+			} break;
 
     	case 'edit':
-            if(!empty($_SESSION["initials"]) && !empty($_POST["postid"])) {
 
-                $perm = mysqli_fetch_assoc(mysqli_querry($conn, "SELECT `id`, `permession` from `employees` WHERE `initiales` ='".$_SESSION["initials"]."' LIMIT 1"));
-                $puid = mysqli_fetch_assoc(mysqli_querry($conn, "SELECT `post`, `userid` from `POST` WHERE `post` ='".$_POST["postid"]."' LIMIT 1"));
-                if($perm["permission"] == -1 || $perm["id"] == $puid["userid"]) {
+            //kjshkjahk
+
+
+            /*if($_POST["submit"]) {
+                if(!empty($_SESSION["initials"]) && !empty($_POST["postid"])) {
+
+                    $perm = mysqli_fetch_assoc(mysqli_query($conn, "SELECT `id`, `permession` from `employees` WHERE `initials` ='".$_SESSION["initials"]."' LIMIT 1"));
+                    $puid = mysqli_fetch_assoc(mysqli_query($conn, "SELECT `post`, `userid` from `POST` WHERE `post` ='".$_POST["postid"]."' LIMIT 1"));
+                    if($perm["permission"] == -1 || $perm["id"] == $puid["userid"]) {
         
-                    if(isset($_POST["delete"]))
-                        if(mysqli_querry($conn, "DELETE FROM `posts` WHERE post = ".$puid["post"]))
-                                 echo "Fjernede opslaget";
-                            else echo "Kunne ikke fjerne opslaget";
+                        if(isset($_POST["delete"]))
+                            if(mysqli_query($conn, "DELETE FROM `posts` WHERE post = ".$puid["post"]))
+                                     echo "<script>window.location.href='".$_POST["redirect"]."'</script>";
+                                else echo "<script>alert('Fejl - kunne ikke fjerne opslag')</script>";
 
-			        else {
-                        if(!empty($_POST["title"]))
-                            if(mysqli_querry($conn, "UPDATE `posts` (`title`) VALUES (".$_POST["title"].") WHERE post = ".$puid["post"]))
-                                 echo "Ændrede titelen";
-                            else echo "Kunne ikke ændre titlen";
-                        if(!empty($_POST["text"]))
-                            if(mysqli_querry($conn, "INSERT INTO `posts` (`text`) VALUES (".$_POST["text"].") WHERE post = ".$puid["post"]))
-                                 echo "Ædrede teksten";
-                            else echo "Kunne ikke ændre teksten";
-                    }
-                } else echo "Du har ikke rettigheder til at ændre dette opslag";
-            } else echo "Fejl"; break;
-
-    	case 'delete':
-            
+			            else {
+                            if(!empty($_POST["title"]))
+                                if(mysqli_query($conn, "UPDATE `posts` (`title`) VALUES (".$_POST["title"].") WHERE post = ".$puid["post"]))
+                                     echo "<script>window.location.href='".$_POST["redirect"]."'</script>";
+                                else echo "<script>alert('Fejl - kunne ikke redigere opslag')</script>";
+                            if(!empty($_POST["text"]))
+                                if(mysqli_query($conn, "INSERT INTO `posts` (`text`) VALUES (".$_POST["text"].") WHERE post = ".$puid["post"]))
+                                     echo "<script>window.location.href='".$_POST["redirect"]."'</script>";
+                                else echo "<script>alert('Fejl - kunne ikke redigere opslag')</script>";
+                        }
+                    } else echo "<script>alert('Du har ikke redigheder til at ændre dette opslag')</script>";
+                }
+            }*/
             break;
-	
-	    default:?>
-            <div>
-                <input type=text">
-            </div>
-        <?php
+
     }
+
+    include_once "overlay.php";
