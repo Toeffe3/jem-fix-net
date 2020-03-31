b9886a23145475ea933fc127ec5a132bfa27edc8
Victor Vejlgaard Schelde Jacobsen <densejevic@gmail.com>
2020-03-31 13:52:33 +0200

    Modifications to overlay

--- functions/editUser.php
+++ functions/editUser.php
@@ -1,20 +1,29 @@
 <?php
 	switch ($_GET["users"]) {
-		case 'new'
-						mysqli_query($conn, "INSERT INTO `employees` (`permission`, `fullname`, `initials`, `password`, `picture`) VALUES ('".$_GET["permission"]."', '".$_GET["name"]."', '".$_GET["initials"]."', '".$_GET["pass"]."', '".$_GET["picture"]."')");
+		case 'new':
+			
+			$action = $url;
+			$inputs = [
+				"name" => ["text", "", "Fulde navn", 1],
+				"initials" => ["text", "", "Initialer", 1],
+				"password" => ["password", "", "Kodeord", 0],
+				"perm" => ["select", $perms, "rettigheder", 1],
+				"redirect" => ["hidden", $prevpage, "", 0],
+				"submit" => ["submit", "Opret", "", 0]
+			];
+			
+			if($_POST["submit"])
+				mysqli_query($conn, "INSERT INTO `employees` (`permission`, `fullname`, `initials`, `password`, `picture`) VALUES ('".$_GET["permission"]."', '".$_GET["name"]."', '".$_GET["initials"]."', '".$_GET["pass"]."', '".$_GET["picture"]."')");
 			break;
 
 		case 'edit':
-						mysqli_query($conn, "UPDATE `employees` SET `password` = '". $_GET["pass"] ."' WHERE `id` = '". $_GET["id"] ."'");
+			mysqli_query($conn, "UPDATE `employees` SET `password` = '". $_GET["pass"] ."' WHERE `id` = '". $_GET["id"] ."'");
 			break;
 
 		case 'delete':
 			mysqli_query($conn, "DELETE FROM `employees` WHERE `id`='".$_GET["id"]."'");
 			break;
 
-		default:
-			//View user
-			//$_GET["id"]
-			break;
 	}
-?>
+
+	include_once "overlay.php";
--- functions/overlay.php
+++ functions/overlay.php
@@ -1,7 +1,7 @@
 <?php ?>
 
-<div id="overlay">
-	<form method="post" action="<?php echo $action; ?>" enctype="multipart/form-data">
+<div id="overlay" onclick="window.location.href='<?php echo $prevpage;?>'">
+	<form method="post" action="<?php echo $action; ?>" enctype="multipart/form-data" onclick="event.stopPropagation()">
 		<table>
 			<?php
 				foreach($inputs as $key => $value) {
@@ -14,33 +14,29 @@
 
 						case "select":
 							echo "<tr><td>".$value[2].': </td><td><select name="'.$key.'">';
-							foreach($value[1] as $okey => $option) {
-								echo '<option value="'.$okey.'">'.$option.'</option>';
-							}
+							foreach($value[1] as $okey => $option) echo '<option value="'.$okey.'">'.$option.'</option>';
 							echo '</select>'.($value[3]==1?"*":"").'<td></tr>';
 							break;
 
-							case "textarea":
-								echo '<tr><td>'.$value[2].': </td></tr><tr><td colspan="2"><textarea name="'.$key.'"></textarea></td></tr>';
-								break;
+						case "textarea":
+							echo '<tr><td>'.$value[2].': </td></tr><tr><td colspan="2"><textarea name="'.$key.'"></textarea></td></tr>';
+							break;
 
-							case "richtext":
-								$enablerichscript = true;
-								echo'<tr><td>'.$value[2].': </td></tr>'.
-										'<tr><td colspan="2"><div style="display: grid; grid-template-columns: repeat(10, 1fr);">'.
-											'<img src="assets/icons/yellow/bold.png" height="50px" onclick="bold()" />'.
-											'<img src="assets/icons/yellow/italic.png" height="50px" onclick="italic()" />'.
-											'<img src="assets/icons/yellow/highlight.png" height="50px" onclick="highlight()" />'.
-											'<img src="assets/icons/yellow/link.png" height="50px" onclick="link()" />'.
-											'<img src="assets/icons/yellow/ol.png" height="50px" onclick="ol()" />'.
-											'<img src="assets/icons/yellow/ul.png" height="50px" onclick="ul()" />'.
-											'<img src="assets/icons/yellow/line.png" height="50px" onclick="line()" />'.
-											'<img src="assets/icons/yellow/h1.png" height="50px" onclick="h1()" />'.
-											'<img src="assets/icons/yellow/h2.png" height="50px" onclick="h2()" />'.
-											'<img src="assets/icons/yellow/h3.png" height="50px" onclick="h3()" />'.
-										'</div></td></tr>'.
-										'<tr><td colspan="2"><textarea class="rich" name="'.$key.'"></textarea></td></tr>';
-								break;
+						case "richtext":
+							$enablerichscript = true;
+							echo'<tr><td>'.$value[2].': </td></tr><tr><td colspan="2"><div style="display: grid; grid-template-columns: repeat(10, 1fr);">'.
+								'<img src="assets/icons/yellow/bold.png" height="50px" onclick="bold()" />'.
+								'<img src="assets/icons/yellow/italic.png" height="50px" onclick="italic()" />'.
+								'<img src="assets/icons/yellow/highlight.png" height="50px" onclick="highlight()" />'.
+								'<img src="assets/icons/yellow/link.png" height="50px" onclick="link()" />'.
+								'<img src="assets/icons/yellow/ol.png" height="50px" onclick="ol()" />'.
+								'<img src="assets/icons/yellow/ul.png" height="50px" onclick="ul()" />'.
+								'<img src="assets/icons/yellow/line.png" height="50px" onclick="line()" />'.
+								'<img src="assets/icons/yellow/h1.png" height="50px" onclick="h1()" />'.
+								'<img src="assets/icons/yellow/h2.png" height="50px" onclick="h3()" />'.
+								'<img src="assets/icons/yellow/h3.png" height="50px" onclick="h6()" />'.
+							'</div></td></tr><tr><td colspan="2"><textarea class="rich" name="'.$key.'"></textarea></td></tr>';
+							break;
 
 						default:
 							echo "<tr>".($value[2]==""?"":"<td>".$value[2].": </td>").'<td'.($value[2]==""?" colspan=2":"").'><input type="'.$value[0].'" name="'.$key.'" value="'.$value[1].'" placeholder="'.$value[2].'" '.($value[3]==1?"required":"").'" />'.($value[3]==1?"*":"").'</td></tr>';
@@ -54,6 +50,18 @@
 </div>
 <?php if(isset($enablerichscript)) { ?>
 	<script type="text/javascript">
+
+		function bold()		{insertAround("\\*\\*");}
+		function italic()	{insertAround("\\_");}
+		function highlight(){insertAround("``");}
+		function link()		{insertAround("\\[insertalink\\]\\(", "\\)",[1,12-sel[1]]);}
+		function line()		{insertAround("", "\n?\n?---\n?");}
+		function h1()		{insertAround("# ", "\n?");}
+		function h3()		{insertAround("### ", "\n?", [4,4]);}
+		function h6()		{insertAround("###### ", "\n?", [7,7]);}
+		function ul()		{insertAround("\n?  * ", "\n?", [5,5]);}
+		function ol()		{insertAround("\n?  x. ", "\n?", [6,6]);}
+
 		var rte = document.getElementsByClassName('rich')[0];
 		rte.onblur = function() {
 			dir = rte.selectionStart<rte.selectionEnd,
@@ -61,41 +69,15 @@
 			txt = [rte.value.slice(0,sel[0]),rte.value.slice(sel[0],sel[1]),rte.value.slice(sel[1])];
 		}
 
-		function bold() {
-			insertInbetween("\\*\\*");
-		}
-
-		function italic() {
-			insertInbetween("\\_");
-		}
-
-		function highlight() {
-			insertInbetween("``");
-		}
-
-		function line() {
-			insertAtStart("---", "\n---\n");
-		}
-
-		function insertInbetween(regexp, chars = regexp.replace(/\\/g,""), offset = 0) {
-			let r=[new RegExp(regexp+"( *?)$"),new RegExp(regexp+"(.*?)"+regexp),new RegExp("^( *?)"+regexp)];
-			if(txt[0].match(r[0])&&txt[2].match(r[2])){rte.value=txt[0].replace(r[0],"$1")+txt[1]+txt[2].replace(r[2],"$1");offset=[-chars.length,-chars.length]}
-			else if(txt[1].match(r[1])){rte.value=txt[0]+txt[1].replace(r[1],"$1")+txt[2];offset=[0,-chars.length-1];}
-			else{rte.value=txt[0]+chars+txt[1]+chars+txt[2];offset=[chars.length,chars.length];}
+		function insertAround(regexpbefore, regexpafter=regexpbefore, offset = undefined, chars = [regexpbefore.replace(/\\/g,"").replace(/\?/g,""),regexpafter.replace(/\\/g,"").replace(/\?/g,"")]) {
+			console.log(regexpafter," --- ", regexpafter.replace(/\?/,""));
+			let r=[new RegExp(regexpbefore+"( *?)$"),new RegExp(regexpbefore+"(.*?)"+regexpafter),new RegExp("^( *?)"+regexpafter)];
+			if(txt[0].match(r[0])&&txt[2].match(r[2])){rte.value=txt[0].replace(r[0],"$1")+txt[1]+txt[2].replace(r[2],"$1");offset?"":offset=[-chars[0].length,-chars[1].length]}
+			else if(txt[1].match(r[1])){rte.value=txt[0]+txt[1].replace(r[1],"$1")+txt[2];offset?"":offset=[0,-chars[1].length-1];}
+			else{rte.value=txt[0]+chars[0]+txt[1]+chars[1]+txt[2];offset?"":offset=[chars[0].length,chars[1].length];}
 			rte.focus();
 			rte.selectionStart=sel[0]+offset[0];
 			rte.selectionEnd=sel[1]+offset[1];
-		}
-
-		function insertAtStart(regexp, chars = regexp.replace(/\\/g,"")) {
-			let offset=-(chars.length), r=[new RegExp(regexp+"( *?)$"),new RegExp(regexp+"(.*?)"+regexp),new RegExp("^( *?)"+regexp)];
-					 if(txt[0].match(r[0]))rte.value=txt[0].replace(r[0],"$1")+txt[1]+txt[2];
-			else if(txt[1].match(r[1]))rte.value=txt[0]+txt[1].replace(r[1],"$1")+txt[2];
-			else if(txt[2].match(r[2]))rte.value=txt[0]+txt[1]+txt[2].replace(r[2],"$1");
-			else{rte.value=txt[0]+chars+txt[1]+txt[2];offset=chars.length;}
-			rte.focus();
-			rte.selectionStart=sel[0]+offset;
-			rte.selectionEnd=sel[1]+offset;
 		}
 	</script>
 <?php } ?>
