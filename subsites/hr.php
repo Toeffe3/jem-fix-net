f16af3df21414f6c5356ed7939439fc6470908ec
Mikkek <54848327+Mikkek@users.noreply.github.com>
2020-04-20 14:04:42 +0200

    Design made

--- subsites/hr.php
+++ subsites/hr.php
@@ -1,4 +1,28 @@
 <?php
 	haveAccessTo(__FILE__);
 ?>
-<h1>HR</h1>
+<div id="hr" class="bg-gray">
+  <div class="taller box" style="border:1px solid #707070">
+		<h1>Formularer</h1>
+	</div>
+
+  <div class="tall box" style="border:1px solid #707070">
+		<h1>Genveje</h1>
+	</div>
+
+  <div class="small box" style="border:1px solid #707070">
+		<h1>Rettighedder og pligter</h1>
+	</div>
+
+	<div class="small box" style="border:1px solid #707070">
+		<h1>MUS</h1>
+	</div>
+
+	<div class="tall box" style="border:1px solid #707070">
+		<h1>Personalegoder</h1>
+	</div>
+
+	<div class="small box" style="border:1px solid #707070">
+		<h1>Persondata forordning</h1>
+	</div>
+</div>
